ab42ecb4510f46b9145403f459e46ab390da52ba
chanwit-y <chanwit.yimneam@hotmail.com>
2022-04-28 10:39:14 +0700

    setup container

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,16 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const commConfig = require("./webpack.common");
+
+const devConfig = {
+  mode: "development",
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+	    index: "index.html",
+    },
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
+};
+
+module.exports = merge(commConfig, devConfig);
